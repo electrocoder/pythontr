75220307696773ca7551f88d69f3857fbb0e083f
Yiğit <yigitsadic@gmail.com>
2011-08-24 21:39:58 +0300

    belgeler views taslağı hazırladım.

--- app/documents/views.py
+++ app/documents/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render, get_object_or_404
+
+from pythontr.app.documents.models import Document
+
+
+def index(request):
+    documents = Document.objects.all()
+    
+    response_dict = {
+        'documents': documents,
+    }
+    
+    return render(request, 'documents/index.html', response_dict)
+    
+    
+def show(request, pk):
+    document = get_object_or_404(Document, pk = pk)
+    
+    response_dict = {
+        'document': document,
+    }
+    
+    return render(request, 'documents/show.html', response_dict)
