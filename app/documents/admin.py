d017e3f038ae07ce9c43c00000b89870735c424a
Yiğit <yigitsadic@gmail.com>
2011-08-24 21:59:30 +0300

    prepopulated_field ekledim.

--- app/documents/admin.py
+++ app/documents/admin.py
@@ -7,15 +7,18 @@ from pythontr.app.documents.models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
+    
+    
     fieldsets = (
-        ('İsim ve dosya', {'fields': ('name', 'docfile')}),
+        ('İsim ve dosya', {'fields': ('name', 'slug', 'docfile')}),
         ('Açıklama ve resim', {'fields': ('description', 'image')}),
         )
 
     list_filter = ('uploaded_at',)
-
     list_display = ('name', 'uploaded_at')
     
     search_fields = ['name', 'description']
+    
+    prepopulated_fields = {'slug': ('name', )}
 
 admin.site.register(Document, DocumentAdmin)
