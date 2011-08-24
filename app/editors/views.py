329668d5c685a6c166a5d71123073946e053c3f6
Yiğit <yigitsadic@gmail.com>
2011-08-24 21:02:05 +0300

    doc string eklendi.

--- app/editors/views.py
+++ app/editors/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 
 
 def profile(request, username):
+    """ Editörün profilini göstermek için kullanılıyor. """
+    
     user = get_object_or_404(User, username = username)
     
     response_dict = {
