d7e688a6f9ca63d40ac24ba6ca30ff21f43d802e
Yiğit <yigitsadic@gmail.com>
2011-08-25 20:53:56 +0300

    url ayarları güncellendi linklerin

--- app/links/urls.py
+++ app/links/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('pythontr.app.links.views',
-                       url(r'^$', 'index', name = 'links_path'),
+                       url(r'^(page/(?P<page>\d+)/)?$', 'index', name = 'links_path'),
 )
     
