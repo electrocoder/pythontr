2673c16fdb430512f35bec5e1d1d97ec3191660b
Yiğit <yigitsadic@gmail.com>
2011-08-26 04:55:59 +0300

    Edited app/users/admin.py via GitHub

--- app/users/admin.py
+++ app/users/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from pythontr.app.users.models import Editor
 
 class EditorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'web')
+    list_filter = ('user__username', 'web', 'bio')
 
 
 admin.site.register(Editor, EditorAdmin)
