db71a0b8bef6be17e7a38be4ad0630ef897d22a7
= <yigitsadic@gmail.com>
2011-08-28 15:46:00 +0300

    ufak bir değişiklik

--- app/links/forms.py
+++ app/links/forms.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.forms import ModelForm
-
 from pythontr.app.links.models import Link
 
 
 class NewLinkForm(ModelForm):
 
-
     class Meta:
-
 
         model = Link
         exclude = ('accepted', )
